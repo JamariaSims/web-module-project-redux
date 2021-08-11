c0190c71f5bdb8f589c4a4ae78189222736a5db5
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-08-10 23:43:38 -0400

    Part 1

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,30 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import movies from "./../data.js";
 
 const initialState = {
-    movies: movies,
-    appTitle: "IMDB Movie Database"
-}
+	movies: movies,
+	favorites: [],
+	appTitle: "IMDB Movie Database",
+};
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case DELETE_MOVIE:
-            return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
-        default:
-            return state;
-    }
-}
+const reducer = (state = initialState, action) => {
+	console.log(action);
+	switch (action.type) {
+		case DELETE_MOVIE:
+			return {
+				movies: state.movies.filter((item) => action.payload !== item.id),
+			};
+		case ADD_MOVIE:
+			const item = state.movies.filter((item) => action.payload === item.id);
+			initialState.favorites.push(item);
+			console.log(initialState);
+			return {
+				movies: state.movies,
+				favorites: initialState.favorites,
+			};
+		default:
+			return state;
+	}
+};
 
 export default reducer;
